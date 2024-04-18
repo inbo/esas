682a9e8b895556a814cda389d18ca0ec76fc9e98
nicolasvanermen <nicolas.vanermen@inbo.be>
2024-04-18 14:13:10 +0200

    Update ESAS functions example script.R

--- R/ESAS functions example script.R
+++ R/ESAS functions example script.R
@@ -1,34 +1,36 @@
-#Example data available at: 
-https://drive.google.com/drive/folders/1mX26tLMC9qZpAIOeAMsS9ng5wPsL1_vH?usp=drive_link
+#Source
+source("./Scripts/ESAS functions.R")
 
 #read tables:
-Campaigns <- Read_ESAS_table(unique_part_of_table_name = "camp", pathway = "./Data/", fileEnc = "UTF-8")
-Samples <- Read_ESAS_table(unique_part_of_table_name = "samp", pathway = "./Data/", fileEnc = "UTF-8")
-Positions <- Read_ESAS_table(unique_part_of_table_name = "pos", pathway = "./Data/", fileEnc = "UTF-8")
-Observations <- Read_ESAS_table(unique_part_of_table_name = "obs", pathway = "./Data/", fileEnc = "UTF-8")
+ESAS_tables <- Read_ESAS_tables(pathway = "./Data/", 
+                                file_encoding = "UTF-8")
+Campaigns <- ESAS_tables[[1]]
+Samples <- ESAS_tables[[2]]
+Positions <- ESAS_tables[[3]]
+Observations <- ESAS_tables[[4]]
+rm(ESAS_tables)
 
 #convert tables:
-ESAS_4_upload <- Convert_ESAS_tables_4_upload(CampaignsTable = Campaigns, 
-                                              SamplesTable = Samples, 
-                                              PositionsTable = Positions, 
-                                              ObservationsTable = Observations, 
-                                              Data_provider = "202", 
-                                              Country = "BE")
+ESAS_4_upload <- Convert_ESAS_tables_4_upload(campaigns_tbl = Campaigns, 
+                                              samples_tbl = Samples, 
+                                              positions_tbl = Positions, 
+                                              observations_tbl = Observations, 
+                                              data_provider = "202", 
+                                              country = "BE")
 
 #write upload table:
 Export_ESAS_upload_matrix(table = ESAS_4_upload, 
                           pathway = "./Output/", 
                           export_name = "ESAS_4_upload", 
-                          fileEnc = "UTF-8")
+                          file_encoding = "UTF-8")
 
 #test by comparing old and new matrix:
-test1 <- read.csv("./Output/ESAS_4_upload_TEST_functions.csv",
-                 sep = "\t", header = F)
+test1 <- read.csv("./Output/ESAS_4_upload_TEST.csv", sep = "\t", header = F)
+test2 <- read.csv("./Output/ESAS_4_upload.csv", sep = "\t", header = F)
 
-test2 <- read.csv("./Output/ESAS_4_upload.csv",
-                  sep = "\t", header = F)
 library(arsenal)
 summary(comparedf(test1, test2))
+rm(test1, test2)
 
-#download observations of Gavia immer from ICES DC ESAS database 
-Observations_Gavia_immer <- Download_ESAS_data(Species=40)
+#download all observations of Gavia immer from ICES DC ESAS database 
+Observations_Gavia_immer <- Download_ESAS_data(species = 40)
