870cd6157529b0d144dac770d96a81a17dcb1fe6
nicolasvanermen <nicolas.vanermen@inbo.be>
2024-04-30 16:51:46 +0200

    Create ESAS functions example Execute distance analysis.R

---
+++ R/ESAS functions example Execute distance analysis.R
@@ -0,0 +1,15 @@
+#Source
+source("./Scripts/ESAS functions.R")
+
+#Read 4 ESAS tables:
+ESAS_TABLES_LIST <- Read_ESAS_Tables(pathway = "./Data/ESAS download 2024 04 30",
+                                     file_encoding = "UTF-8")
+
+#Create ESAS master-table:
+ESAS_TABLE <- Create_ESAS_Table(esas_tables_list = ESAS_TABLES_LIST)
+
+
+#execute distance analysis on selection of species:
+PROBABILITIES <- Detection_Probabilities_Ship_Based_Surveys(esas_table_2_analyse = ESAS_TABLE,
+                                                            species_2_analyse = c(720,6020))
+
