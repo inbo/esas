fa3ec17d9f688aab716806751296d14907325b54
nicolasvanermen <nicolas.vanermen@inbo.be>
2024-04-30 17:57:46 +0200

    Update ESAS functions example Execute distance analysis.R

--- R/ESAS functions example Execute distance analysis.R
+++ R/ESAS functions example Execute distance analysis.R
@@ -9,7 +9,7 @@ ESAS_TABLES_LIST <- Read_ESAS_Tables(pathway = "./Data/ESAS download 2024 04 30"
 ESAS_TABLE <- Create_ESAS_Table(esas_tables_list = ESAS_TABLES_LIST)
 
 
-#execute distance analysis on selection of species:
+#Execute distance analysis on selection of species:
 PROBABILITIES <- Detection_Probabilities_Ship_Based_Surveys(esas_table_2_analyse = ESAS_TABLE,
                                                             species_2_analyse = c(720,6020))
 
