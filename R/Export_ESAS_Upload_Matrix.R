4f1b69fa55acb7197faa54ff0f55633b674bae03
PietrH <pieterhuy@gmail.com>
2025-08-27 15:14:37 +0200

    Add roxygen docs, basic refactor

--- R/Export_ESAS_Upload_Matrix.R
+++ R/Export_ESAS_Upload_Matrix.R
@@ -1,11 +1,39 @@
-#Export upload matrix:
-Export_ESAS_Upload_Matrix <- function(table, pathway, export_name, file_encoding)
-{
-  write.table(table, paste(pathway, export_name, ".csv", sep = ""),
-              sep = "\t",
-              row.names = F,
-              col.names = F,
-              quote = F,
-              fileEncoding = file_encoding)
+#' Write a tab delimited csv file as expected by the ESAS upload module.
+#'
+#' This function exports a data frame to a tab-delimited file without row names,
+#' column names, or quotes, using the specified file encoding.
+#'
+#' @param table A data frame or matrix to be exported as returned by
+#'   [Transform_ESAS_Tables_4_Upload()]
+#' @param path Directory path where the file will be saved.
+#' @param filename Character string specifying the name of the output file
+#'   without the extension.
+#' @param file_encoding Character string specifying the file encoding (default
+#'   is "UTF-8"). See [base::file()] for details on supported encodings.
+#'
+#' @return (invisible) The path to the exported file.
+#' @export
+#' @family upload functions
+#'
+#' @examples
+Export_ESAS_Upload_Matrix <- function(table,
+                                      path,
+                                      filename,
+                                      file_encoding = "UTF-8") {
+  # Export upload matrix
 
+  # Create output path
+  out_path <- paste(path, filename, ".csv", sep = "")
+  # Write table to file
+  write.table(
+    x = table,
+    file = out_path,
+    sep = "\t",
+    row.names = F,
+    col.names = F,
+    quote = F,
+    fileEncoding = file_encoding
+  )
+  # Return path to file
+  invisible(out_path)
 }
