fe3ce143c834dfab867d7ba6f4e3f413d3aa1a09
PietrH <pieterhuy@gmail.com>
2025-08-27 14:18:49 +0200

    Add roxygen docs

--- R/Create_ESAS_Table.R
+++ R/Create_ESAS_Table.R
@@ -1,3 +1,21 @@
+#' Combine the tables from the ESAS Data Model into a single data.frame
+#'
+#' The Analysis functions in this R package expect a single data.frame as input.
+#' This function combines the tables from the ESAS Data Model into a single
+#' data.frame. It performs left joins to ensure that all records from the
+#' CAMPAIGNS, SAMPLES, POSITIONS, and OBSERVATIONS tables are included. It also
+#' renames the Notes columns from the CAMPAIGNS and SAMPLES tables to avoid
+#' naming conflicts.
+#'
+#' @param esas_tables_list A list containing the ESAS tables as data.frames. The list should
+#' include the following named elements: CAMPAIGNS, SAMPLES, POSITIONS, and OBSERVATIONS.
+#'
+#' @return A single data.frame that combines the information from the CAMPAIGNS, SAMPLES,
+#' POSITIONS, and OBSERVATIONS tables.
+#' @export
+#' @family analysis functions
+#'
+#' @examples
 Create_ESAS_Table <- function(esas_tables_list) {
   esas_table <- left_join(
     left_join(
