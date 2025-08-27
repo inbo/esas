ac95643b64680668ba040d5d6b427cbdb573e5fb
PietrH <pieterhuy@gmail.com>
2025-08-27 15:29:26 +0200

    Stylr

--- R/Transform_ESAS_Tables_4_Upload.R
+++ R/Transform_ESAS_Tables_4_Upload.R
@@ -26,16 +26,20 @@
 #' @examples
 #' \dontrun{
 #' ESAS_TABLES_LIST <-
-#'   Read_ESAS_Tables(pathway = "local_folder_with_downloaded_files",
-#'                    file_encoding = "UTF-8")
+#'   Read_ESAS_Tables(
+#'     pathway = "local_folder_with_downloaded_files",
+#'     file_encoding = "UTF-8"
+#'   )
 #'
-#' Transform_ESAS_Tables_4_Upload(campaigns_tbl = ESAS_TABLES_LIST$CAMPAIGNS,
-#'                                samples_tbl = ESAS_TABLES_LIST$SAMPLES,
-#'                                positions_tbl = ESAS_TABLES_LIST$POSITIONS,
-#'                                observations_tbl = ESAS_TABLES_LIST$OBSERVATIONS,
-#'                                data_provider = "202",
-#'                                country = "BE")
-#'}
+#' Transform_ESAS_Tables_4_Upload(
+#'   campaigns_tbl = ESAS_TABLES_LIST$CAMPAIGNS,
+#'   samples_tbl = ESAS_TABLES_LIST$SAMPLES,
+#'   positions_tbl = ESAS_TABLES_LIST$POSITIONS,
+#'   observations_tbl = ESAS_TABLES_LIST$OBSERVATIONS,
+#'   data_provider = "202",
+#'   country = "BE"
+#' )
+#' }
 Transform_ESAS_Tables_4_Upload <- function(campaigns_tbl,
                                            samples_tbl,
                                            positions_tbl,
@@ -59,27 +63,29 @@ Transform_ESAS_Tables_4_Upload <- function(campaigns_tbl,
     dplyr::relocate(RecordType)
 
   file_information <- matrix(nrow = 1, ncol = 18)
-  file_information[1,1:3] <- c("FI", data_provider, country)
+  file_information[1, 1:3] <- c("FI", data_provider, country)
 
   # convert to matrices
   campaigns_matrix <- matrix(nrow = nrow(campaigns_tbl), ncol = 18)
-  campaigns_matrix[,1:6] <- as.matrix(campaigns_tbl)
+  campaigns_matrix[, 1:6] <- as.matrix(campaigns_tbl)
 
   samples_matrix <- matrix(nrow = nrow(samples_tbl), ncol = 18)
-  samples_matrix[,1:16] <- as.matrix(samples_tbl)
+  samples_matrix[, 1:16] <- as.matrix(samples_tbl)
 
   positions_matrix <- matrix(nrow = nrow(positions_tbl), ncol = 18)
-  positions_matrix[,1:16] <- as.matrix(positions_tbl)
+  positions_matrix[, 1:16] <- as.matrix(positions_tbl)
 
   observations_matrix <- matrix(nrow = nrow(observations_tbl), ncol = 18)
-  observations_matrix[,1:18] <- as.matrix(observations_tbl)
+  observations_matrix[, 1:18] <- as.matrix(observations_tbl)
 
   # bind matrices
-  ESAS_2_ICES <- rbind(file_information,
-                       campaigns_matrix,
-                       samples_matrix,
-                       positions_matrix,
-                       observations_matrix)
+  ESAS_2_ICES <- rbind(
+    file_information,
+    campaigns_matrix,
+    samples_matrix,
+    positions_matrix,
+    observations_matrix
+  )
 
   ESAS_2_ICES[is.na(ESAS_2_ICES)] <- ""
   ESAS_2_ICES <- as.data.frame(ESAS_2_ICES)
