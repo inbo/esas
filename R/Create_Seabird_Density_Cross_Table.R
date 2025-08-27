cf988aec9d1a9c17430abd5095c5510abed37605
PietrH <pieterhuy@gmail.com>
2025-08-27 14:35:34 +0200

    Prefix namespace

--- R/Create_Seabird_Density_Cross_Table.R
+++ R/Create_Seabird_Density_Cross_Table.R
@@ -21,53 +21,53 @@
 #' @examples
 Create_Seabird_Density_Cross_Table <- function(esas_table, probabilities, species_selection)
 {
-  esas_table <- esas_table %>% filter(DistanceBins %in% c("0|50|100|200|300"),
+  esas_table <- esas_table %>% dplyr::filter(DistanceBins %in% c("0|50|100|200|300"),
                                       PlatformClass %in% c(30),
                                       Area > 0)
 
   observations_select_fly <- esas_table %>%
-    filter(SpeciesCode %in% species_selection,
+    dplyr::filter(SpeciesCode %in% species_selection,
            ObservationDistance %in% c("F"),
            Transect %in% c("True"),
            !Behaviour %in% c(99))
 
   observations_select_swim <- esas_table %>%
-    filter(SpeciesCode %in% species_selection,
+    dplyr::filter(SpeciesCode %in% species_selection,
            ObservationDistance %in% c("A","B","C","D"),
            Transect %in% c("True"),
            !Behaviour %in% c(99))
 
   probabilities <- probabilities %>%
-    rename(SpeciesCode = Species) %>%
+    dplyr::rename(SpeciesCode = Species) %>%
     select(SpeciesCode, Detection_P_AVG)
 
   observations_select_swim <- left_join(observations_select_swim, probabilities) %>%
-    mutate(Count = Count / Detection_P_AVG)
+    dplyr::mutate(Count = Count / Detection_P_AVG)
 
   observations_select <- rbind(observations_select_fly, observations_select_swim %>% select(!Detection_P_AVG))
 
   base <- esas_table %>% expand(PositionID, species_selection) %>%
-    rename(SpeciesCode = species_selection)
+    dplyr::rename(SpeciesCode = species_selection)
 
   som <- observations_select %>%
-    group_by(PositionID, SpeciesCode) %>%
-    summarise(Count = sum(Count))
+    dplyr::group_by(PositionID, SpeciesCode) %>%
+    dplyr::summarise(Count = sum(Count))
 
   base_som <- left_join(base, som) %>%
-    spread(SpeciesCode, Count, fill = 0) %>%
-    arrange(PositionID)
+    dplyr::spread(SpeciesCode, Count, fill = 0) %>%
+    dplyr::arrange(PositionID)
 
   base_som <- as.data.frame(base_som)
 
   esas_densities_corr <- esas_table %>%
-    distinct(PositionID, Date, Time, Area, Distance, TransectWidth, Latitude, Longitude, SamplingMethod, TargetTaxa)
+    dplyr::distinct(PositionID, Date, Time, Area, Distance, TransectWidth, Latitude, Longitude, SamplingMethod, TargetTaxa)
 
   round_number <- function(x) round(x, digits = 2)
 
-  esas_densities_corr <- left_join(esas_densities_corr, base_som) %>%
-    arrange(Date, Time) %>%
-    mutate_at(paste(species_selection), ~ . / Area) %>%
-    mutate_at(paste(species_selection), round_number)
+  esas_densities_corr <- dplyr::left_join(esas_densities_corr, base_som) %>%
+    dplyr::arrange(Date, Time) %>%
+    dplyr::mutate_at(paste(species_selection), ~ . / Area) %>%
+    dplyr::mutate_at(paste(species_selection), round_number)
 
   return(esas_densities_corr)
 }
