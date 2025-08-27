73ad5bc8334548a4af1832063bd74194a5ca4186
PietrH <pieterhuy@gmail.com>
2025-08-27 15:28:44 +0200

    Declare namespaces

--- R/Transform_ESAS_Tables_4_Upload.R
+++ R/Transform_ESAS_Tables_4_Upload.R
@@ -43,20 +43,20 @@ Transform_ESAS_Tables_4_Upload <- function(campaigns_tbl,
                                            data_provider,
                                            country) {
   campaigns_tbl <- campaigns_tbl %>%
-    mutate(RecordType = "EC") %>%
-    relocate(RecordType)
+    dplyr::mutate(RecordType = "EC") %>%
+    dplyr::relocate(RecordType)
 
   samples_tbl <- samples_tbl %>%
-    mutate(RecordType = "ES") %>%
-    relocate(RecordType)
+    dplyr::mutate(RecordType = "ES") %>%
+    dplyr::relocate(RecordType)
 
   positions_tbl <- positions_tbl %>%
-    mutate(RecordType = "EP") %>%
-    relocate(RecordType)
+    dplyr::mutate(RecordType = "EP") %>%
+    dplyr::relocate(RecordType)
 
   observations_tbl <- observations_tbl %>%
-    mutate(RecordType = "EO") %>%
-    relocate(RecordType)
+    dplyr::mutate(RecordType = "EO") %>%
+    dplyr::relocate(RecordType)
 
   file_information <- matrix(nrow = 1, ncol = 18)
   file_information[1,1:3] <- c("FI", data_provider, country)
