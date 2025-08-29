d12baa6adeee45d21f8ffc9d39a379516fc7f525
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2025-08-29 14:59:51 +0200

    Add test placeholders

--- tests/testthat/test-Calculate_Detection_P_Ship_Based_Surveys.R
+++ tests/testthat/test-Calculate_Detection_P_Ship_Based_Surveys.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Calculate_Detection_P_Ship_Based_Surveys() returns error when none of the selected species are in the input table", {
+
+})
+
+test_that("Calculate_Detection_P_Ship_Based_Surveys() warning on missing species", {
+
 })
