1077644cbc43f4114c8794a01650b8a749f9b94b
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2025-08-29 14:59:41 +0200

    Add test placeholders

--- tests/testthat/test-Create_Seabird_Density_Cross_Table.R
+++ tests/testthat/test-Create_Seabird_Density_Cross_Table.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# Read ESAS data from file
+data_path <- system.file("extdata", "ESAS_0827343782", package = "esas")
+esas_table <- Read_ESAS_Tables(data_path) |>
+  Create_ESAS_Table()
+
+test_that("Create_Seabird_Density_Cross_Table() returns data.frame", {
+
+})
+
+test_that("Create_Seabird_Density_Cross_Table() returns expected columns", {
+
 })
