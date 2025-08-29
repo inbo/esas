e80c4e868e27c1a05a1396294492a84b4fea0cd5
PietrH <pieterhuy@gmail.com>
2025-08-29 09:51:35 +0200

    Add basic unit tests, failing.

--- tests/testthat/test-Transform_ESAS_Tables_4_Upload.R
+++ tests/testthat/test-Transform_ESAS_Tables_4_Upload.R
@@ -1,9 +1,117 @@
-test_that("Transform_ESAS_Tables_4_Upload() returns data.frame with expected cols", {
+test_that("Transform_ESAS_Tables_4_Upload() returns data.frame", {
   skip("test incomplete")
-  expect_s3_class(Transform_ESAS_Tables_4_Upload(),
-                  "data.frame")
+  # Read some ESAS data to transform
+  data_path <- system.file("extdata", "ESAS_0827343782", package = "esas")
+  esas_tables <- Read_ESAS_Tables(data_path)
+
+  # Transform the tables to single data.frame
+  transformed_esas_tables <-
+    Transform_ESAS_Tables_4_Upload(
+      campaigns_tbl = esas_tables$CAMPAIGNS,
+      samples_tbl = esas_tables$SAMPLES,
+      positions_tbl = esas_tables$POSITIONS,
+      observations_tbl = esas_tables$OBSERVATIONS,
+      data_provider = "202",
+      country = "BE"
+    )
+
+  expect_s3_class(transformed_esas_tables, "data.frame")
+})
+
+test_that("Transform_ESAS_Tables_4_Upload() returns correct columns", {
+  # Read some ESAS data to transform
+  data_path <- system.file("extdata", "ESAS_0827343782", package = "esas")
+  esas_tables <- Read_ESAS_Tables(data_path)
+
+  # Transform the tables to single data.frame
+  transformed_esas_tables <-
+    Transform_ESAS_Tables_4_Upload(
+      campaigns_tbl = esas_tables$CAMPAIGNS,
+      samples_tbl = esas_tables$SAMPLES,
+      positions_tbl = esas_tables$POSITIONS,
+      observations_tbl = esas_tables$OBSERVATIONS,
+      data_provider = "202",
+      country = "BE"
+    )
 
   expect_named(
-
+    transformed_esas_tables,
+    c(
+      "DataRightsHolder",
+      "Country",
+      "CampaignID",
+      "DataAccess",
+      "StartDate",
+      "EndDate",
+      "Notes",
+      "SampleID",
+      "Date",
+      "PlatformCode",
+      "PlatformClass",
+      "PlatformSide",
+      "PlatformHeight",
+      "TransectWidth",
+      "SamplingMethod",
+      "PrimarySampling",
+      "TargetTaxa",
+      "DistanceBins",
+      "UseOfBinoculars",
+      "NumberOfObservers",
+      "PositionID",
+      "Time",
+      "Latitude",
+      "Longitude",
+      "Distance",
+      "Area",
+      "WindForce",
+      "Visibility",
+      "Glare",
+      "SunAngle",
+      "CloudCover",
+      "Precipitation",
+      "IceCover",
+      "ObservationConditions",
+      "ObservationID",
+      "GroupID",
+      "Transect",
+      "SpeciesCodeType",
+      "SpeciesCode",
+      "SpeciesScientificName",
+      "SpeciesEnglishName",
+      "WormsAphiaID",
+      "WormsScientificName",
+      "Count",
+      "ObservationDistance",
+      "LifeStage",
+      "Moult",
+      "Plumage",
+      "Sex",
+      "TravelDirection",
+      "Prey",
+      "Association",
+      "Behaviour",
+      "RecordType"
+    )
   )
+})
+
+test_that("Transform_ESAS_Tables_4_Upload() returns expected number of rows", {
+  # Number of rows returned should be sum of rows of input tables + 1 for file
+  # information
+})
+
+test_that("Transform_ESAS_Tables_4_Upload() returns error on invalid country", {
+
+})
+
+test_that("Transform_ESAS_Tables_4_Upload() returns error on invalid data_provider", {
+
+})
+
+test_that("Transform_ESAS_Tables_4_Upload() returns error on missing input table", {
+
+})
+
+test_that("Transform_ESAS_Tables_4_Upload() returns error on invalid input table", {
+  # Missing columns
 })
