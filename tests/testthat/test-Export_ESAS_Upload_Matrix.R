a78009d197f627f4c4eaed6275487a177e370e08
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2025-08-29 14:41:27 +0200

    Use withr to clean up after testing

--- tests/testthat/test-Export_ESAS_Upload_Matrix.R
+++ tests/testthat/test-Export_ESAS_Upload_Matrix.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# Create a tempoary directory with withr so it'll be cleaned as the tests finish
+testing_tempdir <- withr::local_tempdir(pattern = "esas_tests",
+                     tmpdir = tempdir())
+
 })
